9805fee962003c3e0ac28ecbe5db36d7cf7419b0
Joey <jiangjun12321@sina.com>
2018-04-14 11:03:17 +0800

    mixin mu with ant

--- src/pages/users/login.js
+++ src/pages/users/login.js
@@ -1,43 +1,133 @@
-
 import React from 'react';
-import PropTypes from 'prop-types';
+
+// Ant
+import 'antd/dist/antd.css'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+// Mu
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import Typography from 'material-ui/Typography';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
+import MuButton from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+// input
+import MuInput, { InputLabel, InputAdornment } from 'material-ui/Input';
+import Visibility from 'material-ui-icons/Visibility';
+import VisibilityOff from 'material-ui-icons/VisibilityOff';
+
+
+
+const FormItem = Form.Item;
+
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+
+  // state = {
+  //   amount: '',
+  //   password: '',
+  //   weight: '',
+  //   weightRange: '',
+  //   showPassword: false,
+  // };
+
+  // handleChange = prop => event => {
+  //   this.setState({ [prop]: event.target.value });
+  // };
+
+  // handleMouseDownPassword = event => {
+  //   event.preventDefault();
+  // };
+
+  // handleClickShowPassword = () => {
+  //   this.setState({ showPassword: !this.state.showPassword });
+  // };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem style={{maxWidth: 260, margin: 'auto'}}>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: '请填入用户名' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="手机号或邮箱" />
+          )}
+        </FormItem>
+        <FormItem style={{maxWidth: 260, margin: 'auto'}}>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: '请填入登录密码' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
+          )}
+
+        </FormItem>
+        <FormItem style={{ textAlign: 'center' }}>
+          <MuButton variant="raised" color="secondary" style={{marginTop: 10,width: 260}} type="submit">
+            Let Me In
+          </MuButton>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
 
 function TabContainer({ children, dir = "x-reverse" }) {
   return (
     <div>
       <Paper style={{ width: 500, paddingTop: 16, paddingBottom: 16, marginTop: 2 * 3 }} elevation={4}>
-        <Typography variant="headline" component="h3">
-          {children}
-        </Typography>
-        <Typography component="p">
-          Paper can be used to build surface or other elements for your application.
-        </Typography>
+        <WrappedNormalLoginForm />
       </Paper>
     </div>
   );
 }
 
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-// TabContainer.propTypes = {
-//   children: PropTypes.node.isRequired,
-// };
-
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    marginTop: theme.spacing.unit * 3,
-    backgroundColor: theme.palette.background.paper,
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+  },
+  textFieldRoot: {
+    padding: 0,
+    'label + &': {
+      marginTop: theme.spacing.unit * 3,
+    },
+  },
+  textFieldInput: {
+    borderRadius: 4,
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 12px',
+    width: 'calc(100% - 24px)',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    '&:focus': {
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+  textFieldFormLabel: {
+    fontSize: 18,
   },
 });
+
 
 class SimpleTabs extends React.Component {
   state = {
@@ -53,8 +143,7 @@ class SimpleTabs extends React.Component {
     const { value } = this.state;
 
     return (
-
-      <Grid container className={{ flexGrow: 1 }}>
+      <Grid container style={{ flexGrow: 1, marginTop: 30 }}>
         <Grid item xs={12}>
           <Grid
             container
@@ -65,20 +154,16 @@ class SimpleTabs extends React.Component {
           >
 
             <Grid item>
-
               <div className={classes.root}>
                 <Paper style={{ width: 500 }}>
-                  <Tabs value={value} onChange={this.handleChange}
-
-                    fullWidth
-                  >
-                    <Tab label="Item One" />
-                    <Tab label="Item Two" />
+                  <Tabs value={value} onChange={this.handleChange} fullWidth>
+                    <Tab label="登录" />
+                    <Tab label="注册" />
                   </Tabs>
                 </Paper>
 
-                {value === 0 && <TabContainer >Item One</TabContainer>}
-                {value === 1 && <TabContainer >Item Two</TabContainer>}
+                {value === 0 && <TabContainer >登录</TabContainer>}
+                {value === 1 && <TabContainer >注册</TabContainer>}
               </div>
 
             </Grid>
@@ -91,21 +176,117 @@ class SimpleTabs extends React.Component {
   }
 }
 
-SimpleTabs.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(SimpleTabs);
 
 
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { withStyles } from 'material-ui/styles';
+// import AppBar from 'material-ui/AppBar';
+// import Tabs, { Tab } from 'material-ui/Tabs';
+// import Typography from 'material-ui/Typography';
+// import Paper from 'material-ui/Paper';
+// import Grid from 'material-ui/Grid';
+
+// import Input, { InputLabel } from 'material-ui/Input';
+// import TextField from 'material-ui/TextField';
+// import { FormControl } from 'material-ui/Form';
+// import purple from 'material-ui/colors/purple';
+
 // const styles = theme => ({
-//   root: {
-//     backgroundColor: theme.palette.background.paper,
-//     width: 500,
+//   container: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//   },
+//   formControl: {
+//     margin: theme.spacing.unit,
+//   },
+//   inputLabelFocused: {
+//     color: purple[500],
+//   },
+//   inputUnderline: {
+//     '&:after': {
+//       backgroundColor: purple[500],
+//     },
+//   },
+//   textFieldRoot: {
+//     padding: 0,
+//     'label + &': {
+//       marginTop: theme.spacing.unit * 3,
+//     },
+//   },
+//   textFieldInput: {
+//     borderRadius: 4,
+//     backgroundColor: theme.palette.common.white,
+//     border: '1px solid #ced4da',
+//     fontSize: 16,
+//     padding: '10px 12px',
+//     width: 'calc(100% - 24px)',
+//     transition: theme.transitions.create(['border-color', 'box-shadow']),
+//     '&:focus': {
+//       borderColor: '#80bdff',
+//       boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+//     },
+//   },
+//   textFieldFormLabel: {
+//     fontSize: 18,
 //   },
 // });
 
-// class FullWidthTabs extends React.Component {
+
+// function TabContainer({ children, dir = "x-reverse" }) {
+//   return (
+//     <div>
+//       <Paper style={{ width: 500, paddingTop: 16, paddingBottom: 16, marginTop: 2 * 3 }} elevation={4}>
+//           {/* <FormControl className={classes.formControl}>
+//             <InputLabel
+//               FormLabelClasses={{
+//                 focused: classes.inputLabelFocused,
+//               }}
+//               htmlFor="custom-color-input"
+//             >
+//               Name
+//             </InputLabel>
+//             <Input
+//               classes={{
+//                 underline: classes.inputUnderline,
+//               }}
+//               id="custom-color-input"
+//             />
+//           </FormControl> */}
+//       </Paper>
+//     </div>
+//   );
+// }
+
+// TabContainer.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
+
+// // TabContainer.propTypes = {
+// //   children: PropTypes.node.isRequired,
+// // };
+
+// const styles = theme => ({
+//   root: {
+//     flexGrow: 1,
+//     marginTop: theme.spacing.unit * 3,
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// });
+
+// class SimpleTabs extends React.Component {
 //   state = {
 //     value: 0,
 //   };
@@ -114,45 +295,56 @@ export default withStyles(styles)(SimpleTabs);
 //     this.setState({ value });
 //   };
 
-//   handleChangeIndex = index => {
-//     this.setState({ value: index });
-//   };
-
 //   render() {
 //     const { classes, theme } = this.props;
+//     const { value } = this.state;
 
 //     return (
-//       <div className={classes.root}>
-//         <AppBar position="static" color="default">
-//           <Tabs
-//             value={this.state.value}
-//             onChange={this.handleChange}
-//             indicatorColor="primary"
-//             textColor="primary"
-//             fullWidth
+
+//       <Grid container className={{ flexGrow: 1 }}>
+//         <Grid item xs={12}>
+//           <Grid
+//             container
+//             spacing={16}
+//             alignItems={'center'}
+//             direction={'row'}
+//             justify={'center'}
 //           >
-//             <Tab label="Item One" />
-//             <Tab label="Item Two" />
-//             <Tab label="Item Three" />
-//           </Tabs>
-//         </AppBar>
-//         <SwipeableViews
-//           axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-//           index={this.state.value}
-//           onChangeIndex={this.handleChangeIndex}
-//         >
-//           <TabContainer dir={theme.direction}>Item One</TabContainer>
-//           <TabContainer dir={theme.direction}>Item Two</TabContainer>
-//           <TabContainer dir={theme.direction}>Item Three</TabContainer>
-//         </SwipeableViews>
-//       </div>
+
+//             <Grid item>
+//               <div className={classes.root}>
+//                 <Paper style={{ width: 500 }}>
+//                   <Tabs value={value} onChange={this.handleChange} fullWidth>
+//                     <Tab label="Item One" />
+//                     <Tab label="Item Two" />
+//                   </Tabs>
+//                 </Paper>
+
+//                 {value === 0 && <TabContainer >登录</TabContainer>}
+//                 {value === 1 && <TabContainer >注册</TabContainer>}
+//               </div>
+
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       </Grid>
+
+
 //     );
 //   }
 // }
 
-// FullWidthTabs.propTypes = {
+// SimpleTabs.propTypes = {
 //   classes: PropTypes.object.isRequired,
-//   theme: PropTypes.object.isRequired,
 // };
 
-// export default withStyles(styles, { withTheme: true })(FullWidthTabs);
+// export default withStyles(styles)(SimpleTabs);
+
+
+// // const styles = theme => ({
+// //   root: {
+// //     backgroundColor: theme.palette.background.paper,
+// //     width: 500,
+// //   },
+// // });
+
